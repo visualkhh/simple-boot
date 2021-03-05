6d95761952a8e697adcb07363bc7bfc784bd123e
khh <visualkhh@gmail.com>
2021-03-05 11:35:54 +0900

    move Directory

--- simple-front/test/node/index.js
+++
@@ -1,9 +0,0 @@
-// const fs = require('fs');
-// var user = require('user');
-var a = require;
-a.wow = 55;
-console.log(a)
-// // console.log("---", process, 'filesystem', fs)
-//
-
-// console.log(require.main);
--- simple-front/types/html.d.ts
+++
@@ -1,8 +0,0 @@
-declare module "*.html" {
-    const content: string;
-    export default content;
-}
-// declare module "*!text" {
-//     const Content: string;
-//     export default Content;
-// }
